28c55501a2df01312c688c0500eb14905cf4b29c
Isaac <isaac@ux430ua.hub>
2020-10-15 22:08:17 +1300

    ,
    Update home page on main

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function toggle_menu(x) {
     if ($(x).hasClass("change")) {
         $('body').css('overflow', 'hidden');
         $(".menu_container").toggleClass("menu_container_open");
+        $(".masthead, .content").toggleClass('blur');
         if ($('.fixed-top.text-center h2').hasClass('op0')) {
             $('.fixed-top.text-center h2').removeClass('op0');
         };
@@ -11,7 +12,6 @@ function toggle_menu(x) {
             function () {
                 $(".menu_container").toggle();
                 $(".sidebar").toggleClass("menu_open");
-                $("body").toggleClass("body_menu_open");
                 
                 setTimeout(
                     function () {
@@ -23,13 +23,13 @@ function toggle_menu(x) {
     } else {
         $('body').css('overflow', 'visible');
         $(".sidebar_list").toggleClass("op1");
+        $(".masthead, .content").toggleClass('blur');
         if ($(document).scrollTop() > 300) {
             $('.fixed-top.text-center h2').addClass('op0');
         };
         setTimeout(
             function () {
                 $(".sidebar").toggleClass("menu_open");
-                $("body").toggleClass("body_menu_open");
             }, 300);
         setTimeout(
             function () {
