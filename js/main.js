d0b99f065dbf8b7cb27fdfc6fc6fad167a85a7b6
Isaac <isaac@ux430ua.student.uod.otago.ac.nz>
2020-10-22 15:40:48 +1300

    more mobile friendly css

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(document).ready( function() {
             };
         },  // This option accepts a callback function. The function will be called before the page moves.
         afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-        loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+        loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
         keyboard: true,                  // You can activate the keyboard controls
         responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
                                          // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
